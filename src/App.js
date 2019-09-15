611bc15d2095868e55fd0fc84b3089c4570d0710
soleilyasmina <mounlasolomon@gmail.com>
2019-09-15 16:45:56 -0400

    rendered basic data to dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import initialData from './assets/data';
 
 function App() {
+  const [data, setData] = useState(initialData);
+
   return (
     <div className="App">
+      { data.columnOrder.map(columnId => {
+        const column = data.columns[columnId];
+        const tasks = column.taskIds.map((taskId) => {
+          return data.tasks[taskId];
+        });
+        return column.title;
+      }) }
     </div>
   );
 }
---
+++ src/assets/data.js
@@ -0,0 +1,16 @@
+export default {
+  tasks: {
+    'task-1': { id: 'task-1', content: 'Take out the garbage' },
+    'task-2': { id: 'task-2', content: 'Make sweet tunes' },
+    'task-3': { id: 'task-3', content: 'Rock out' },
+    'task-4': { id: 'task-4', content: 'Take down the government' },
+  },
+  columns: {
+    'column-1' : {
+      id: 'column-1',
+      title: 'To do',
+      taskIds: ['task-1', 'task-2', 'task-3', 'task-4'],
+    }
+  },
+  columnOrder: ['column-1'],
+}
