28c5ca208921ae10d1b00d839f63d63abe9bad19
soleilyasmina <mounlasolomon@gmail.com>
2019-09-15 17:03:33 -0400

    wow! tasks

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Task from './Task';
 
 const Column = (props) => {
   return (
     <div className="container">
       <h3 className="title">{props.column.title}</h3>
-      <div className="tasklist">Tasks go here.</div>
+      <div className="tasklist">{props.tasks.map(task => {
+        return <Task key={task.id} task={task} />
+      })}</div>
     </div>
   )
 }
---
+++ src/components/Task.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Task = (props) => (
+  <div className="task-container">{props.task.content}</div>
+)
+
+export default Task;
