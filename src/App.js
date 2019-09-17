afe004dd851ca328f914c4994c8436090051a90d
soleilyasmina <mounlasolomon@gmail.com>
2019-09-17 14:18:25 -0400

    added onDragEnd callback

--- src/App.js
+++ src/App.js
@@ -8,7 +8,33 @@ function App() {
   const [data, setData] = useState(initialData);
 
   const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
 
+    if (!destination) {
+      return;
+    }
+
+    if (destination.droppableId === source.droppableI &&
+      destination.index === source.index) {
+      return;
+    }
+
+    const column = data.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = { ...column, taskIds: newTaskIds };
+
+    const newState = {
+      ...data,
+      columns: {
+        ...data.columns,
+        [newColumn.id]: newColumn,
+      }
+    }
+
+    setData(newState);
   }
 
   return (
